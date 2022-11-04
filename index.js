6ac24d76f7f341439b4beb6f2c31e09563adbc3e
Krzywdek19 <61578292+Krzywdek19@users.noreply.github.com>
2022-11-04 20:21:56 +0100

    zegar

--- index.js
+++ index.js
@@ -1,4 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-console.log('njnjk');
+
+const elementRoot = ReactDOM.createRoot(document.getElementById('root'));
+
+function User(name, secondName) {
+	this.name = name;
+	this.secondName = secondName;
+	this.getFullName = function () {
+		return this.name + ' ' + this.secondName;
+	};
+}
+
+let user = new User('Jakub', 'Krzywdzinski');
+
+let i = 0;
+
+let hours = 0;
+let minutes = 0;
+let second = 0;
+
+function workingTime() {
+	second++;
+
+	if (second > 59) {
+		second = 0;
+		minutes++;
+	}
+	if (minutes > 59) {
+		minutes = 0;
+		hours++;
+	}
+
+	function get(value) {
+		if (value < 10) {
+			return '0' + value;
+		} else {
+			return value;
+		}
+	}
+
+	let element = (
+		<h1>
+			Aplikacja działa {get(hours)}:{get(minutes)}:{get(second)}
+		</h1>
+	);
+	elementRoot.render(element);
+}
+
+setInterval(workingTime, 1000);
